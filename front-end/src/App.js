37a0c48b1b3eba007c3eca0d59c55540be6ab969
Abdellatif Al Kerde <boudikerde0@gmail.com>
2023-03-07 12:48:55 +0200

    adding header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,23 @@ import NavBar from "./components/navbar/index";
 import { Outlet } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import "./App.css";
+import Header from "./components/header/index";
 
 export default function App() {
   return (
-    <Grid container style={{ flexWrap: "nowrap" }}>
+    <div style={{ display: "flex" }}>
+      {/* //{" "}
+      <Grid container style={{ flexWrap: "nowrap" }}>
+        // <NavBar />
+        // <Header />
+        // <Outlet />
+        //{" "}
+      </Grid> */}
       <NavBar />
-      <Outlet />
-    </Grid>
+      <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
+        <Header />
+        <Outlet />
+      </div>
+    </div>
   );
 }
