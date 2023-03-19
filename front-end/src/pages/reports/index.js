7c230ec2bf2d99eb5145463168ae01a1336d0c2e
Abdellatif Al Kerde <boudikerde0@gmail.com>
2023-03-19 19:46:40 +0200

    fixing report popup problem

--- front-end/src/pages/reports/index.js
+++ front-end/src/pages/reports/index.js
@@ -1,63 +1,61 @@
-import React,{useState , useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import Report from "./repot";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { TextField } from "@mui/material";
-import CloseIcon from "@mui/icons-material/Close"
+import CloseIcon from "@mui/icons-material/Close";
 import MainButton from "../../components/main-button/index";
-import axios from "axios"
+import axios from "axios";
 import Loding from "../../components/loding/Loding";
-
+import Popup from "../../components/pop-up/Popup";
+import Swal from "sweetalert2";
 
 function ReportChart(props) {
-  
-  const [visibleAdd , setVisibleAdd] = useState(false)
+  const [visibleAdd, setVisibleAdd] = useState(false);
 
-  const [showEdit , setShowedit]= useState(false)
+  const [showEdit, setShowedit] = useState(false);
 
-  const [dataReport ,setDataReport]=useState(null)
+  const [dataReport, setDataReport] = useState(null);
 
-  const [addDataReport , setAddDataReport]=useState({
-    report:'',
-    type_report:'',
-    admins_id:'',
-    categories_id:'',
-    start_date:'',
-    end_date:''
-  })
+  const [addDataReport, setAddDataReport] = useState({
+    report: "",
+    type_report: "",
+    admins_id: "",
+    categories_id: "",
+    start_date: "",
+    end_date: "",
+  });
 
-  const [editDataReport , seteditDataReport]=useState({
-    report:'',
-    type_report:'',
-    admins_id:'',
-    categories_id:'',
-    start_date:'',
-    end_date:''
-  })
-  
-  const [editId , setEditId] = useState(null)
-  
-  const getId =(id)=>{
-    setEditId(id)
-  }
-  console.log(editId)
+  const [editDataReport, seteditDataReport] = useState({
+    report: "",
+    type_report: "",
+    admins_id: "",
+    categories_id: "",
+    start_date: "",
+    end_date: "",
+  });
+
+  const [editId, setEditId] = useState(null);
+
+  const getId = (id) => {
+    setEditId(id);
+  };
+  console.log(editId);
   const visibleEdit = () => {
     if (showEdit === false) {
-        setShowedit(true);
+      setShowedit(true);
     } else {
-        setShowedit(false);
+      setShowedit(false);
     }
-};
+  };
 
-  const show = ()=>{
-    if(visibleAdd === false){
-      setVisibleAdd(true)
+  const show = () => {
+    if (visibleAdd === false) {
+      setVisibleAdd(true);
+    } else {
+      setVisibleAdd(false);
     }
-    else{
-      setVisibleAdd(false)
-    }
-  }
-
+  };
 
   //get data
 
@@ -73,7 +71,6 @@ function ReportChart(props) {
     fetchData();
   }, []);
 
-
   //post data
 
   const handleChange = (e) => {
@@ -84,22 +81,28 @@ function ReportChart(props) {
     });
   };
 
-  const handelSubmit=()=>{
+  const handelSubmit = () => {
     const data = {
-      report:addDataReport.report,
-      type_report:addDataReport.type_report,
-      admins_id:addDataReport.admins_id,
-      categories_id:addDataReport.categories_id,
-      start_date:addDataReport.start_date,
-      end_date:addDataReport.end_date,
-    }
-    axios.post("http://127.0.0.1:8000/api/report",data).then((response)=>{
-      console.log(response)
-      fetchData()
-    })
-  }
+      report: addDataReport.report,
+      type_report: addDataReport.type_report,
+      admins_id: addDataReport.admins_id,
+      categories_id: addDataReport.categories_id,
+      start_date: addDataReport.start_date,
+      end_date: addDataReport.end_date,
+    };
+    axios.post("http://127.0.0.1:8000/api/report", data).then((response) => {
+      console.log(response);
+      fetchData();
+    });
+    Swal.fire({
+      icon: "success",
+      title: "Report Added successfully",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
 
-  //edit 
+  //edit
 
   const handleEditChange = (e) => {
     const value = e.target.value;
@@ -108,162 +111,207 @@ function ReportChart(props) {
       [e.target.name]: value,
     });
   };
-  const handeleditSubmit=()=>{
+  const handeleditSubmit = () => {
     const data = {
-      report:editDataReport.report,
-      type_report:editDataReport.type_report,
-      admins_id:editDataReport.admins_id,
-      categories_id:editDataReport.categories_id,
-      start_date:editDataReport.start_date,
-      end_date:editDataReport.end_date,
-    }
-    axios.patch(`http://127.0.0.1:8000/api/report/${editId}`,data).then((response)=>{
-      console.log(response)
-      fetchData()
-    })
-  } 
-   
+      report: editDataReport.report,
+      type_report: editDataReport.type_report,
+      admins_id: editDataReport.admins_id,
+      categories_id: editDataReport.categories_id,
+      start_date: editDataReport.start_date,
+      end_date: editDataReport.end_date,
+    };
+    axios
+      .patch(`http://127.0.0.1:8000/api/report/${editId}`, data)
+      .then((response) => {
+        console.log(response);
+        fetchData();
+      });
+    Swal.fire({
+      icon: "success",
+      title: "Report Updated successfully",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
 
-  if(!dataReport){return <div style={{display:"flex" , justifyContent:'center', height:'80%',alignItems:'center'}}><Loding/></div>}
+  if (!dataReport) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "80%",
+          alignItems: "center",
+        }}
+      >
+        <Loding />
+      </div>
+    );
+  }
   return (
-    <div style={{width:"100%" , height:"84vh",position:"relative"}}>
-    <div className="report" >
- 
-    <div className="add-categories"style={{width:'90%'}}>
-          <MainButton name="Add Category" onClick={show}  />
+    <div style={{ width: "100%", height: "84vh", position: "relative" }}>
+      <div className="report">
+        <div className="add-categories" style={{ width: "90%" }}>
+          <MainButton name="Add Category" onClick={show} />
         </div>
-      
+
         <div className="report-container">
-        {dataReport.map((ele)=>{
-          return <Report useEffect={fetchData} getId={getId}   visibleEdit={visibleEdit} id={ele.id}  report={ele.report} type={ele.type_report} admin={ele.admins_id} category={ele.categories_id} start_date={ele.start_date} end_date={ele.end_date}/>
-        })}
+          {dataReport.map((ele) => {
+            return (
+              <Report
+                useEffect={fetchData}
+                getId={getId}
+                visibleEdit={visibleEdit}
+                id={ele.id}
+                report={ele.report}
+                type={ele.type_report}
+                admin={ele.admins_id}
+                category={ele.categories_id}
+                start_date={ele.start_date}
+                end_date={ele.end_date}
+              />
+            );
+          })}
         </div>
-      
-    </div>
-        {visibleAdd&&<div
-            className="reports-close-popup"
+      </div>
+      {visibleAdd && (
+        <Popup>
+          <div
+            className="currencies-close-popup"
+            onClick={() => {
+              setVisibleAdd(false);
+            }}
           >
-          
+            <CloseIcon />
+          </div>
           <Box
             className="add-categories-box"
             component="form"
             noValidate
             autoComplete="off"
-            sx={{width:'500px' , backgroundColor:'white'}}
+            // sx={{ width: "500px", backgroundColor: "white" }}
           >
             <h2>Add Report</h2>
-            <p onClick={show} style={{fontSize:'40px' , marginTop:'2px',marginBottom:'2px' }}>x</p>
-            <form className="pop-up-form">
-              <TextField
-                id="outlined-controlled"
-                label="Add Report"
-                name="report"
-                onChange={handleChange}
-              />
-              <TextField
-                id="outlined-uncontrolled"
-                label="Add Type"
-                name="type_report"
-                onChange={handleChange}
-              />
-              <TextField
-                id="outlined-read-only"
-                label="Admin"
-                name="admins_id"
-                onChange={handleChange}
-              />
-              <TextField
-                id="outlined-read-only"
-                label="Category"
-                name="categories_id"
-                onChange={handleChange}
-              />
-              <TextField
-                id="outlined-read-only"
-                label="Start date"
-                name="start_date"
-                onChange={handleChange}
-              />
-              <TextField
-                id="outlined-read-only"
-                label="End date"
-                name="end_date"
-                onChange={handleChange}
-              />
-              <Button
-                variant="contained"
-                disableElevation
-                style={{ height: 55 }}
-                sx={{ backgroundColor: "#3d0066" }}
-                onClick={handelSubmit}
-              >
-                Submit
-              </Button>
-            </form>
+            <TextField
+              id="outlined-controlled"
+              label="Add Report"
+              name="report"
+              onChange={handleChange}
+            />
+            <TextField
+              id="outlined-uncontrolled"
+              label="Add Type"
+              name="type_report"
+              onChange={handleChange}
+            />
+            <TextField
+              id="outlined-read-only"
+              label="Admin"
+              name="admins_id"
+              onChange={handleChange}
+            />
+            <TextField
+              id="outlined-read-only"
+              label="Category"
+              name="categories_id"
+              onChange={handleChange}
+            />
+            <TextField
+              id="outlined-read-only"
+              label="Start date"
+              name="start_date"
+              onChange={handleChange}
+            />
+            <TextField
+              id="outlined-read-only"
+              label="End date"
+              name="end_date"
+              onChange={handleChange}
+            />
+            <Button
+              variant="contained"
+              disableElevation
+              style={{ height: 55 }}
+              sx={{ backgroundColor: "#3d0066" }}
+              onClick={() => {
+                handelSubmit();
+                setVisibleAdd(false);
+              }}
+            >
+              Submit
+            </Button>
           </Box>
-          </div>}
-          {showEdit&&<div
-            className="reports-close-popup"
+          {/* </div> */}
+        </Popup>
+      )}
+      {showEdit && (
+        <Popup>
+          <div
+            className="currencies-close-popup"
+            onClick={() => {
+              setShowedit(false);
+            }}
           >
-          
+            <CloseIcon />
+          </div>
           <Box
             className="add-categories-box"
             component="form"
             noValidate
             autoComplete="off"
-            sx={{width:'500px' , backgroundColor:'white'}}
           >
             <h2>Edit Report</h2>
-            <p  onClick={visibleEdit} style={{fontSize:'40px' , marginTop:'2px',marginBottom:'2px' }}>x</p>
-            <form className="pop-up-form">
-              <TextField
-                id="outlined-controlled"
-                label="Add Report"
-                name="report"
-                onChange={handleEditChange}
-              />
-              <TextField
-                id="outlined-uncontrolled"
-                label="Add Type"
-                name="type_report"
-                onChange={handleEditChange}
-              />
-              <TextField
-                id="outlined-read-only"
-                label="Admin"
-                name="admins_id"
-                onChange={handleEditChange}
-              />
-              <TextField
-                id="outlined-read-only"
-                label="Category"
-                name="categories_id"
-                onChange={handleEditChange}
-              />
-              <TextField
-                id="outlined-read-only"
-                label="Start date"
-                name="start_date"
-                onChange={handleEditChange}
-              />
-              <TextField
-                id="outlined-read-only"
-                label="End date"
-                name="end_date"
-                onChange={handleEditChange}
-              />
-              <Button
-                variant="contained"
-                disableElevation
-                style={{ height: 55 }}
-                sx={{ backgroundColor: "#3d0066" }}
-                onClick={handeleditSubmit}
-              >
-                Submit
-              </Button>
-            </form>
+            <TextField
+              id="outlined-controlled"
+              label="Add Report"
+              name="report"
+              onChange={handleEditChange}
+            />
+            <TextField
+              id="outlined-uncontrolled"
+              label="Add Type"
+              name="type_report"
+              onChange={handleEditChange}
+            />
+            <TextField
+              id="outlined-read-only"
+              label="Admin"
+              name="admins_id"
+              onChange={handleEditChange}
+            />
+            <TextField
+              id="outlined-read-only"
+              label="Category"
+              name="categories_id"
+              onChange={handleEditChange}
+            />
+            <TextField
+              id="outlined-read-only"
+              label="Start date"
+              name="start_date"
+              onChange={handleEditChange}
+            />
+            <TextField
+              id="outlined-read-only"
+              label="End date"
+              name="end_date"
+              onChange={handleEditChange}
+            />
+            <Button
+              variant="contained"
+              disableElevation
+              style={{ height: 55 }}
+              sx={{ backgroundColor: "#3d0066" }}
+              onClick={() => {
+                handeleditSubmit();
+                setShowedit(false);
+              }}
+            >
+              Submit
+            </Button>
           </Box>
-          </div>}
+        </Popup>
+      )}
     </div>
   );
 }
--- front-end/src/pages/reports/repot.js
+++ front-end/src/pages/reports/repot.js
@@ -1,91 +1,118 @@
 import React, { useState } from "react";
 import "./reports.css";
 import { CSSTransition } from "react-transition-group";
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { Button } from "@mui/material";
 import axios from "axios";
-import Reports from "./index.js"
+import Reports from "./index.js";
+import Swal from "sweetalert2";
 
 function Report(props) {
-    const [report, setReport] = useState(false);
-    const [padding, setPadding] = useState("20px");
-    const[id ,setId]=useState(null)
+  const [report, setReport] = useState(false);
+  const [padding, setPadding] = useState("20px");
+  const [id, setId] = useState(null);
 
-    // console.log(id)
-    
-    const visible = () => {
-        if (report === false || padding === "20px") {
-            setReport(true);
-            setPadding(0);
-        } else {
-            setReport(false);
-            setPadding("20px");
-        }
-    };
+  // console.log(id)
 
-    //delete
-    const handelDelete =()=>{
-        axios.delete(`http://127.0.0.1:8000/api/report/${id}`).then((response)=>{
-          console.log(response)
-          props.useEffect()
-        })
-      }
+  const visible = () => {
+    if (report === false || padding === "20px") {
+      setReport(true);
+      setPadding(0);
+    } else {
+      setReport(false);
+      setPadding("20px");
+    }
+  };
 
-    return (
-        <div>
-            {false&&<Reports />}
-        <div className="card-report" onMouseEnter={visible} onMouseLeave={visible}>
-            <div
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    display: "flex",
-                    justifyContent: "center",
-                    paddingBottom: `${padding}`,
-                }}
-            >
-                <div className="title-report" style={{display:'flex' , justifyContent:"space-between"}}>
-                    <div>
-                    <h3>Report : {props.report}</h3>
-                    <h4>Type : {props.type}</h4>
-                    </div>
-                    <div style={{width:"35%" ,display:"flex" ,justifyContent:'space-around' ,alignItems:'center'}}>
-                    <Button sx={{minWidth:20 ,color: "#3d0066",}} onClick={props.visibleEdit} onMouseEnter={()=>{props.getId(props.id)}} >
-                    <EditIcon/>
-                    </Button>
-                    
-                    <Button sx={{minWidth:20 ,color: "#3d0066",}} onMouseEnter={()=>{setId(props.id)}} onClick={handelDelete}  >
-                    <DeleteIcon/>
-                    </Button>
-                    
-                    </div>
-                </div>
+  //delete
+  const handelDelete = () => {
+    axios.delete(`http://127.0.0.1:8000/api/report/${id}`).then((response) => {
+      console.log(response);
+      props.useEffect();
+    });
+    Swal.fire("Deleted!", "Your admin has been deleted.", "success");
+  };
+
+  return (
+    <div>
+      {false && <Reports />}
+      <div
+        className="card-report"
+        onMouseEnter={visible}
+        onMouseLeave={visible}
+      >
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            paddingBottom: `${padding}`,
+          }}
+        >
+          <div
+            className="title-report"
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <div>
+              <h3>Report : {props.report}</h3>
+              <h4>Type : {props.type}</h4>
             </div>
+            <div
+              style={{
+                width: "35%",
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+              }}
+            >
+              <Button
+                sx={{ minWidth: 20, color: "#3d0066" }}
+                onClick={props.visibleEdit}
+                onMouseEnter={() => {
+                  props.getId(props.id);
+                }}
+              >
+                <EditIcon />
+              </Button>
 
-            {report && (
-                <div className="more-info">
-                    <p>
-                        <strong>admin : </strong>
-                        {props.admin}
-                    </p>
-                    <p>
-                        <strong>category : </strong>
-                        {props.category}
-                    </p>
-                    <p>
-                        <strong>Start date : </strong>
-                        {props.start_date}
-                    </p>
-                    <p>
-                        <strong>End date : </strong>
-                        {props.end_date}
-                    </p>
-                </div>
-            )}
+              <Button
+                sx={{ minWidth: 20, color: "#3d0066" }}
+                onMouseEnter={() => {
+                  setId(props.id);
+                }}
+                onClick={handelDelete}
+              >
+                <DeleteIcon />
+              </Button>
+            </div>
+          </div>
         </div>
-        </div>
-    );
+
+        {report && (
+          <div className="more-info">
+            <p>
+              <strong>admin : </strong>
+              {props.admin}
+            </p>
+            <p>
+              <strong>category : </strong>
+              {props.category}
+            </p>
+            <p>
+              <strong>Start date : </strong>
+              {props.start_date}
+            </p>
+            <p>
+              <strong>End date : </strong>
+              {props.end_date}
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Report;
